738bd6b33d5aa9971c3587c953df2be3c8f6fcae
SunSeekerX <1647800606@qq.com>
2022-03-23 18:07:02 +0800

    feat: temp

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -10,7 +10,7 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
   'package.json': ['prettier --write'],
-  '*.vue': ['prettier --write', 'stylelint --fix --allow-empty-input'],
-  '*.{scss,less,styl,css,html}': ['stylelint --fix --allow-empty-input', 'prettier --write'],
+  // '*.vue': ['prettier --write', 'stylelint --fix --allow-empty-input'],
+  // '*.{scss,less,styl,css,html}': ['stylelint --fix --allow-empty-input', 'prettier --write'],
   '*.md': ['prettier --write'],
 }
