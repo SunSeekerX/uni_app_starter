132753b7c03583106b635fe81e1a54923e24dd76
SunSeekerX <1647800606@qq.com>
2022-06-21 18:20:51 +0800

    feat: upushy-client

---
+++ src/uni_modules/upushy-client/src/lang/en_US.js
@@ -0,0 +1,57 @@
+/**
+ * 英语(美国)
+ * @author: SunSeekerX
+ * @Date: 2021-05-05 22:13:18
+ * @LastEditors: SunSeekerX
+ * @LastEditTime: 2021-08-10 21:50:21
+ */
+
+export default {
+  is_request_updating: 'Checking for updates...',
+  is_silent_updating: 'Silent update in progress...',
+  is_update_success: 'The update has been completed, and the APP needs to be restarted to take effect',
+  is_updating: 'Updating...',
+
+  notice_update_closed: 'Update closed',
+  notice_no_update_url: 'No update url',
+  notice_no_project_id: 'No project id',
+  notice_unknown_error: 'Unknown error',
+  notice_install_fail: 'Install fail',
+  notice_native_update_required: 'Native update required',
+  notice_wgt_update_required: 'Wgt update required',
+  notice_no_update: 'No update',
+  notice_api_fail: 'Api request fail',
+  notice_api_error: 'Api request error',
+  notice_no_download_url: 'No download url',
+  notice_is_updating: 'Updating...',
+
+  notice_on_new_version: 'New version ',
+  notice_no_update_now: 'Cancel',
+  notice_update_now: 'Update',
+  notice_updating: 'Updating...',
+  notice_update_app: 'Upgrade',
+  notice_update_cancel: 'Cancel',
+  notice_update_background: 'Background',
+  notice_update_close: 'Close',
+
+  console_get_uuid_fail: 'Get uuid fail',
+  console_api_response: 'Api response',
+  console_unknown_error: 'UnKnown error',
+  console_update_type_not_support: 'Update type not support',
+  console_is_install: 'Installing...',
+  console_install_success: 'Install success!',
+  console_download_fail: 'Download fail!',
+  console_prepare_download: 'Prepare...',
+  console_downloading: 'Downloading...',
+  console_has_downloaded: '',
+  console_download_cancel: 'Canceled',
+  console_start_request_update: 'Start request update...',
+  console_silent_update_success: 'Silent update success',
+  console_download_task_request: 'Request download url',
+  console_download_task_connected: 'Download url connected',
+  console_download_task_success: 'Download success',
+  console_config_update_success: 'Config update success',
+  console_invalid_console_type: 'Invalid console type',
+  console_init_config: 'Init config',
+  console_get_info_start: 'Starting get system info',
+}
